eae12b34624e2710ab762b2024c95c707e8f3f0d
Eduardo Nava <ed.nava.valencia@gmail.com>
2025-09-13 00:35:35 -0700

    feat: server boilerplate added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
+const logger = require('./middleware/logger');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(logger);
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+// Server Routes
+app.use('/api', routes);
+
+// Client routes
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
