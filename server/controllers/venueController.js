1edd94ddee5f5e69ab89819b34700ada8d3084b3
Benjamin Noble <tworedfrog2@gmail.com>
2025-09-15 15:59:22 -0700

    created base routes for venues and changed user schema (#9)

---
+++ server/controllers/venueController.js
@@ -0,0 +1,21 @@
+const venue = require('../models/venue');
+const bcrypt = require('bcrypt');
+
+exports.getVenues = async (req, res) => {
+  try {
+    const venues = await venue.find();
+    res.status(200).json(venues);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch venues', details: err.message });
+  }
+};
+
+exports.createVenue = async (req, res) => {
+  try {
+    const newVenue = new venue(req.body);
+    const savedVenue = await newVenue.save();
+    res.status(201).json(savedVenue);
+  } catch (err) {
+    res.status(400).json({ error: 'Failed to create venue', details: err.message });
+  }
+};
