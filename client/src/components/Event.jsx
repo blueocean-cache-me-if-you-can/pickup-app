ce1fd4f7814a33054591e17a15659e85c4f5d2ac
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-09-17 13:18:05 -0700

    MyEvents component (#22)
    * optional grayscale flag isPast/grayscale
    
    * Update filters and sort to accommodate upcoming and past events for MyEvents
    
    * gitignore update

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -4,30 +4,38 @@ import EventCard from './EventCard';
 import IconInfo from './IconInfo';
 import AttendeesRatio from './AttendeesRatio';
 
-function Event({ event }) {
+function Event({ event, isPast = false }) {
   const expandEvent = () => {
     // TODO: trigger event details modal
     console.log(`Event ${event.title} expanded`);
   };
+  const grayscale = isPast || new Date(event.date) < new Date();
+  const street = event.location.split(',')[0];
   return (
-    <Grid m='md' onClick={expandEvent}>
-      <Grid.Col span={3}>
-        <EventCard event={event} />
-      </Grid.Col>
-      <Grid.Col span={9}>
-        <Stack maw={900} mah={300}>
-          <AttendeesRatio players={event.players} maxPlayers={event.maxPlayers} />
-          <Group spacing='xs'>
-            <IconInfo iconType='time' infoText={new Date(event.date).toLocaleString()} size='sm' />
-            <IconInfo iconType='location' infoText={event.location} size='sm' />
-            <IconInfo iconType='owner' infoText={event.owner.name} size='sm' />
-          </Group>
-          <div style={{ borderTop: '1px solid #e0e0e0', margin: '12px 0' }} />
-          <Text fw={700} size='lg'>{event.title}</Text>
-          <Text size='sm'>{event.description}</Text>
-        </Stack>
-      </Grid.Col>
-    </Grid>
+    <div style={{ filter: grayscale ? 'grayscale(100%)' : 'none' }}>
+      <Grid m='md' onClick={expandEvent}>
+        <Grid.Col span={3}>
+          <EventCard event={event} />
+        </Grid.Col>
+        <Grid.Col span={9}>
+          <Stack maw={900} mah={300}>
+            <AttendeesRatio
+              players={event.players}
+              maxPlayers={event.maxPlayers}
+              grayscale={grayscale}
+            />
+            <Group spacing='xs'>
+              <IconInfo iconType='time' infoText={new Date(event.date).toLocaleString()} size='sm' grayscale={grayscale} />
+              <IconInfo iconType='location' infoText={street} size='sm' grayscale={grayscale} />
+              <IconInfo iconType='owner' infoText={event.owner.name} size='sm' grayscale={grayscale} />
+            </Group>
+            <div style={{ borderTop: '1px solid #e0e0e0', margin: '12px 0' }} />
+            <Text fw={700} size='lg' style={{ color: grayscale ? '#888' : undefined }}>{event.title}</Text>
+            <Text size='sm' style={{ color: grayscale ? '#888' : undefined }}>{event.description}</Text>
+          </Stack>
+        </Grid.Col>
+      </Grid>
+    </div>
   );
 }
 
