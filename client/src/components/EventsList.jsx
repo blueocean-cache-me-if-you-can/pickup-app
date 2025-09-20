c78b8eeffb2e5f427db5d40153864a913fae01c7
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-09-20 11:17:33 -0700

    Fix/displays (#52)
    * remove inner scroll and update imgs
    
    * lifting current event state

--- client/src/components/EventsList.jsx
+++ client/src/components/EventsList.jsx
@@ -6,35 +6,41 @@ import EventDetails from './EventDetails';
 import Event from './Event';
 
 function EventsList({
-  currentUserId, events, activities, intensities, skillLevels, height = '70vh',
+  currentUserId, events, activities, intensities, skillLevels,
 }) {
+  const [eventList, setEventList] = useState(events);
   const [page, setPage] = useState(1);
-  const eventsPerPage = 5;
-  const totalPages = Math.ceil(events.length / eventsPerPage);
+  const eventsPerPage = 2;
+  const totalPages = Math.ceil(eventList.length / eventsPerPage);
   const startIdx = (page - 1) * eventsPerPage;
   const endIdx = startIdx + eventsPerPage;
-  const paginatedEvents = events.slice(startIdx, endIdx);
+  const paginatedEvents = eventList.slice(startIdx, endIdx);
 
   const handlePrev = () => setPage((p) => Math.max(p - 1, 1));
   const handleNext = () => setPage((p) => Math.min(p + 1, totalPages));
 
+  // Handler to update a single event in the event list
+  const handleSetCurrentEvent = (updatedEvent) => {
+    setEventList((prevEvents) =>
+      prevEvents.map((ev) => (ev._id === updatedEvent._id ? updatedEvent : ev))
+    );
+  };
+
   return (
     <Box>
-      <ScrollArea h={height}>
-        <Stack>
-          {paginatedEvents.map((event) => (
-            <EventDetails
-              key={event._id}
-              event={event}
-              activities={activities}
-              intensities={intensities}
-              skillLevels={skillLevels}
-              currentUserId={currentUserId}
-            />
-          ))}
-        </Stack>
-
-      </ScrollArea>
+      <Stack>
+        {paginatedEvents.map((event) => (
+          <EventDetails
+            key={event._id}
+            event={event}
+            activities={activities}
+            intensities={intensities}
+            skillLevels={skillLevels}
+            currentUserId={currentUserId}
+            setCurrentEvent={handleSetCurrentEvent}
+          />
+        ))}
+      </Stack>
       {totalPages > 1 && (
         <Group justify='center' mt='md'>
           <Button onClick={handlePrev} disabled={page === 1} variant='outline'>
